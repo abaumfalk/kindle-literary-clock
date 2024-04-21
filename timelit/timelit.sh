56fe4fea13bda6957dad068ae19f06d4feab3741
Arno Baumfalk <a.baumfalk@web.de>
2024-04-21 12:52:28 +0200

    clear screen only once per hour to save battery (and still avoid ghosting)

--- timelit/timelit.sh
+++ timelit/timelit.sh
@@ -22,8 +22,11 @@ ThisMinuteImage=$( find /mnt/us/timelit/images/quote_$MinuteOTheDay* 2>/dev/null
 
 echo $ThisMinuteImage > /mnt/us/timelit/clockisticking
 
-# clear the screen
-eips -c
+# clear the screen every hour to avoid ghosting
+minute=$(printf "%s" "$MinuteOfTheDay" | tail -c 2)
+if [ "$minute" = "00" ]; then 
+	eips -c
+fi
 
 # show that image
 eips -g $ThisMinuteImage
